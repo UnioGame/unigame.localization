72e95d03103c0ad09ba85c8d435e974a801dbec4
Mefodei <mefodei.link@gmail.com>
2023-11-15 16:05:05 +0700

    - fix time utils usaage

--- Runtime/TimeUtils.cs
+++ Runtime/TimeUtils.cs
@@ -7,17 +7,25 @@
     using UniGame.Utils.Runtime;
     using UniCore.Runtime.ProfilerTools;
     using UnityEngine.Localization.SmartFormat.Utilities;
+    using UnityEngine.Pool;
 
     public static class TimeUtils
     {
+        private const string TimerFormat4 = "{0} {1}:{2}:{3}";
+        private const string TimerFormat3 = "{0}:{1}:{2}";
+        private const string TimerFormat2 = "{0}:{1}";
+        private const string TimerFormat1 = "{0}";
+        
         public const TimeSpanFormatOptions DefaultOptions = TimeSpanFormatOptions.Abbreviate|TimeSpanFormatOptions.LessThanOff|
                                                             TimeSpanFormatOptions.TruncateFill|TimeSpanFormatOptions.RangeSeconds|
                                                             TimeSpanFormatOptions.RangeHours;
         public const TimeSpanFormatOptions DefaultOptionsWithoutSeconds = TimeSpanFormatOptions.Abbreviate|
                                                                           TimeSpanFormatOptions.LessThanOff|TimeSpanFormatOptions.TruncateFill|
                                                                           TimeSpanFormatOptions.RangeMinutes|TimeSpanFormatOptions.RangeHours;
-        
-        private static Regex _tokenRegex = new Regex(@"\{[^\{\}]+\}");
+            
+        private static Regex _tokenRegex = new(@"\{[^\{\}]+\}");
+        private static List<int> _units = new();
+        private static List<int> _resultUnits = new();
 
         private static TimeZoneInfo easterStandardTimezone = 
             TimeZoneInfo.CreateCustomTimeZone(
@@ -26,7 +34,7 @@
                 "America/NewYork", 
                 "America/NewYork");
         
-        public static readonly DateTime StartTime = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc);
+        public static readonly DateTime StartTime = new(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc);
 
         public static DateTime EstStartTime => TimeZoneInfo.ConvertTimeFromUtc(StartTime, easterStandardTimezone);
 
@@ -71,28 +79,40 @@
 
         public static string Format(string format, TimeSpan timeSpan)
         {
-            var units  = new List<int> {timeSpan.Days, timeSpan.Hours, timeSpan.Minutes, timeSpan.Seconds};
+            _units.Clear();
+            _resultUnits.Clear();
+            
+            _units.Add(timeSpan.Days);
+            _units.Add(timeSpan.Hours);
+            _units.Add(timeSpan.Minutes);
+            _units.Add(timeSpan.Seconds);
 
             var tokens = _tokenRegex.Matches(format).Count;
-            if (tokens > units.Count)
+            if (tokens > _units.Count)
             {
-                GameLog.LogError($"Input string requires more parameters. Format: {format}, submitted parameters count: {units.Count}");
+                GameLog.LogError($"Input string requires more parameters. Format: {format}, submitted parameters count: {_units.Count}");
                 return timeSpan.ToString(@"hh\:mm\:ss");
             }
 
-            var firstValIndex = units.FindIndex(u => u != 0);
-            units = firstValIndex == -1 
-                ? Enumerable.Repeat(0, tokens).ToList() 
-                : units.GetRange(Math.Min(firstValIndex, units.Count - tokens), tokens);
+            var firstValIndex = _units.FindIndex(u => u != 0);
+            var isEmpty = firstValIndex < 0;
             
+            var index = isEmpty ? 0 : Math.Min(firstValIndex, _units.Count - tokens);
+            
+            for (var i = index; i < _units.Count; i++)
+            {
+                _resultUnits.Add(_units[i]);
+            }
+
             return tokens switch
             {
-                1 => string.Format(format, units[0]),
-                2 => string.Format(format, units[0], units[1]),
-                3 => string.Format(format, units[0], units[1], units[2]),
-                4 => string.Format(format, units[0], units[1], units[2], units[3]),
+                1 => string.Format(format, _resultUnits[0]),
+                2 => string.Format(format, _resultUnits[0], _resultUnits[1]),
+                3 => string.Format(format, _resultUnits[0], _resultUnits[1], _resultUnits[2]),
+                4 => string.Format(format, _resultUnits[0], _resultUnits[1], _resultUnits[2], _resultUnits[3]),
                 _ => format
             };
         }
+        
     }
 }
